b029f718d81c39c6688c7f7bb133013585de8abf
lifeng <lifeng9204@qq.com>
2024-01-13 18:17:35 +0800

    feat: fetch data from debug.ReadBuildInfo

--- info.go
+++ info.go
@@ -1,5 +1,10 @@
 package build_info
 
+import (
+	"encoding/json"
+	"runtime/debug"
+)
+
 type info struct {
 	GitCommit string
 	GoVersion string
@@ -17,6 +22,30 @@ var (
 )
 
 func Info() info {
+	build, ok := debug.ReadBuildInfo()
+	if ok {
+		goVersion = build.GoVersion
+
+		settings := map[string]string{}
+		for _, s := range build.Settings {
+			settings[s.Key] = s.Value
+		}
+
+		if v, ok := settings["GOOS"]; ok {
+			os = v
+		}
+		if v, ok := settings["GOARCH"]; ok {
+			arch = v
+		}
+
+		if v, ok := settings["vcs"]; ok && v == "git" {
+			gitCommit = settings["vcs.revision"][:8]
+			if modified, ok := settings["vcs.modified"]; ok && modified == "true" {
+				gitCommit += "-dirty"
+			}
+		}
+	}
+
 	return info{
 		GitCommit: gitCommit,
 		GoVersion: goVersion,
@@ -24,4 +53,9 @@ func Info() info {
 		Arch:      arch,
 		BuildTime: buildTime,
 	}
+}
+
+func (i info) String() string {
+	b, _ := json.MarshalIndent(i, "", "  ")
+	return string(b)
 }
